cae6743a15dd61750f9fed9ce6912484be0990a9
Oleksandr Bilenko <oleksandr.bilenko@c24.de>
2026-03-06 08:06:27 +0100

    Libs: af unix socket

---
+++ src/libs/socket/af-unix-socket.cpp
@@ -0,0 +1,93 @@
+#include "af-unix-socket.h"
+#include <cstring>
+#include <sys/socket.h>
+#include <unistd.h>
+
+namespace lib {
+
+AfUnixSocketState AfUnixUdpSocket::connect() {
+  // Prev applicable state
+  close();
+
+  state_ = initializeSock();
+  if (state_ != AfUnixSocketState::READY) {
+    return state_.value();
+  }
+
+  auto *readdr = reinterpret_cast<sockaddr *>(&sockAddr_);
+  if (::connect(sockFd_, readdr, sizeof(sockAddr_)) < 0) {
+    state_ = AfUnixSocketState::NO_SOCK_CONNECT;
+    return state_.value();
+  }
+
+  setActualBufSize(SO_RCVBUF);
+
+  state_ = AfUnixSocketState::CONNECT;
+  return state_.value();
+}
+
+AfUnixSocketState AfUnixUdpSocket::bind() {
+  // Prev applicable state
+  close();
+
+  state_ = initializeSock();
+  if (state_ != AfUnixSocketState::READY) {
+    return state_.value();
+  }
+
+  auto *readdr = reinterpret_cast<sockaddr *>(&sockAddr_);
+  if (::bind(sockFd_, readdr, sizeof(sockAddr_)) < 0) {
+    state_ = AfUnixSocketState::NO_SOCK_BIND;
+    return state_.value();
+  }
+
+  setActualBufSize(SO_SNDBUF);
+
+  state_ = AfUnixSocketState::BIND;
+  return state_.value();
+}
+
+AfUnixSocketState AfUnixUdpSocket::initializeSock() {
+  if (sockPath_.size() > sizeof(sockAddr_.sun_path) - 1) {
+    return AfUnixSocketState::SOCK_PRECONDITIONS;
+  }
+
+  sockFd_ = socket(AF_UNIX, SOCK_DGRAM, 0);
+  if (sockFd_ < 0) {
+    return AfUnixSocketState::NO_SOCK;
+  }
+
+  sockAddr_.sun_family = AF_UNIX;
+  std::strncpy(sockAddr_.sun_path, sockPath_.c_str(),
+               sizeof(sockAddr_.sun_path) - 1);
+
+  return AfUnixSocketState::READY;
+}
+
+void AfUnixUdpSocket::setActualBufSize(int option) {
+  int actual{};
+  socklen_t len = sizeof(actual);
+  getsockopt(sockFd_, SOL_SOCKET, option, &actual, &(len));
+  actualBufSize_ = actual;
+}
+
+int AfUnixUdpSocket::sockFd() const { return sockFd_; }
+
+std::optional<int> AfUnixUdpSocket::actualBufSize() const {
+  return actualBufSize_;
+}
+
+AfUnixUdpSocket::~AfUnixUdpSocket() { close(); }
+
+void AfUnixUdpSocket::close() {
+  if (sockFd_ >= 0) {
+    ::close(sockFd_);
+    sockFd_ = -1;
+  }
+  if (state_ == AfUnixSocketState::BIND) {
+    unlink(sockPath_.c_str());
+  }
+  state_ = AfUnixSocketState::INITIAL;
+}
+
+} // namespace lib
---
+++ src/libs/socket/af-unix-socket.h
@@ -0,0 +1,59 @@
+
+#ifndef LIB_SOCKET_AF_UNIX_H
+#define LIB_SOCKET_AF_UNIX_H
+
+#include <optional>
+#include <string>
+#include <sys/un.h>
+
+namespace lib {
+
+enum class AfUnixSocketState {
+  INITIAL,
+  SOCK_PRECONDITIONS,
+  NO_SOCK,
+  NO_SOCK_CONNECT,
+  NO_SOCK_BIND,
+  READY,
+  CONNECT,
+  BIND
+};
+
+class AfUnixUdpSocket {
+public:
+  explicit AfUnixUdpSocket(std::string sockPath)
+      : sockPath_(std::move(sockPath)){};
+
+  // As client
+  AfUnixSocketState connect();
+
+  // As server
+  AfUnixSocketState bind();
+
+  int sockFd() const;
+
+  std::optional<int> actualBufSize() const;
+
+  ~AfUnixUdpSocket();
+  AfUnixUdpSocket(const AfUnixUdpSocket &) = delete;
+  AfUnixUdpSocket &operator=(const AfUnixUdpSocket &) = delete;
+  AfUnixUdpSocket(AfUnixUdpSocket &&other) = delete;
+  AfUnixUdpSocket &operator=(AfUnixUdpSocket &&other) = delete;
+
+private:
+  std::string sockPath_;
+  int sockFd_ = -1;
+  std::optional<AfUnixSocketState> state_{AfUnixSocketState::INITIAL};
+  ::sockaddr_un sockAddr_{};
+  std::optional<int> actualBufSize_{std::nullopt};
+
+  AfUnixSocketState initializeSock();
+
+  void close();
+
+  void setActualBufSize(int option);
+};
+
+} // namespace lib
+
+#endif
