e2191d8926b9e1f65ef8418616f5e94b0785d7e2
Oleksandr Bilenko <oleksandr.bilenko@c24.de>
2026-03-06 08:48:29 +0100

    Libs: udp socket

---
+++ src/libs/socket/udp-socket.cpp
@@ -0,0 +1,90 @@
+#include "udp-socket.h"
+#include <sys/socket.h>
+#include <unistd.h>
+
+namespace lib {
+
+UdpSocketState UdpSocket::bind() {
+  // Prev applicable state
+  close();
+
+  state_ = initialize();
+  if (state_ != UdpSocketState::READY) {
+    return state_;
+  }
+
+  int reuse{1};
+  setsockopt(sockFd_, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+
+  sockaddr *readdr = reinterpret_cast<sockaddr *>(&sockAddrIn_);
+  if (::bind(sockFd_, readdr, sizeof(sockAddrIn_)) < 0) {
+    state_ = UdpSocketState::NO_SOCK_BIND;
+    return state_;
+  }
+
+  setActualBufSize(SO_RCVBUF);
+
+  state_ = UdpSocketState::BIND;
+  return state_;
+}
+
+UdpSocketState UdpSocket::connect() {
+  // Prev applicable state
+  close();
+
+  state_ = initialize();
+  if (state_ != UdpSocketState::READY) {
+    return state_;
+  }
+
+  sockaddr *readdr = reinterpret_cast<sockaddr *>(&sockAddrIn_);
+  if (::connect(sockFd_, readdr, sizeof(sockAddrIn_)) < 0) {
+    state_ = UdpSocketState::NO_SOCK_CONNECT;
+    return state_;
+  }
+
+  setActualBufSize(SO_RCVBUF);
+
+  state_ = UdpSocketState::CONNECT;
+  return state_;
+}
+
+UdpSocketState UdpSocket::initialize() {
+  if (sockPort_ == 0) {
+    return UdpSocketState::PRECONDITIONS;
+  }
+
+  sockFd_ = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sockFd_ < 0) {
+    return UdpSocketState::NO_SOCK;
+  }
+
+  sockAddrIn_.sin_family = AF_INET;
+  sockAddrIn_.sin_addr.s_addr = INADDR_ANY;
+  sockAddrIn_.sin_port = htons(sockPort_);
+
+  return UdpSocketState::READY;
+}
+
+void UdpSocket::setActualBufSize(int option) {
+  int actual{};
+  socklen_t len = sizeof(actual);
+  getsockopt(sockFd_, SOL_SOCKET, option, &actual, &(len));
+  actualBufSize_ = actual;
+}
+
+int UdpSocket::sockFd() const { return sockFd_; }
+
+std::optional<int> UdpSocket::actualBufSize() const { return actualBufSize_; }
+
+UdpSocket::~UdpSocket() { close(); }
+
+void UdpSocket::close() {
+  if (sockFd_ >= 0) {
+    ::close(sockFd_);
+    sockFd_ = -1;
+  }
+  state_ = UdpSocketState::INITIAL;
+}
+
+} // namespace lib
---
+++ src/libs/socket/udp-socket.h
@@ -0,0 +1,58 @@
+
+#ifndef LIB_SOCKET_UDP_H
+#define LIB_SOCKET_UDP_H
+
+#include <cstdint>
+#include <netinet/in.h>
+#include <optional>
+
+namespace lib {
+
+enum class UdpSocketState {
+  INITIAL,
+  PRECONDITIONS,
+  NO_SOCK,
+  NO_SOCK_CONNECT,
+  NO_SOCK_BIND,
+  READY,
+  CONNECT,
+  BIND
+};
+
+class UdpSocket {
+public:
+  explicit UdpSocket(uint16_t sockPort) : sockPort_(sockPort){};
+
+  // As client
+  UdpSocketState connect();
+
+  // As server
+  UdpSocketState bind();
+
+  int sockFd() const;
+
+  std::optional<int> actualBufSize() const;
+
+  ~UdpSocket();
+  UdpSocket(const UdpSocket &) = delete;
+  UdpSocket &operator=(const UdpSocket &) = delete;
+  UdpSocket(UdpSocket &&other) = delete;
+  UdpSocket &operator=(UdpSocket &&other) = delete;
+
+private:
+  uint16_t sockPort_;
+  int sockFd_ = -1;
+  UdpSocketState state_{UdpSocketState::INITIAL};
+  sockaddr_in sockAddrIn_{};
+  std::optional<int> actualBufSize_{std::nullopt};
+
+  UdpSocketState initialize();
+
+  void close();
+
+  void setActualBufSize(int option);
+};
+
+} // namespace lib
+
+#endif
